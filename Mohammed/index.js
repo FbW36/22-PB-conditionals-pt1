b3ed389d2a34d70d9facd41781452a4533fa9ddf
mashraee <m.almashraee@gmail.com>
2020-10-28 22:48:35 +0100

    Exercise 22 -refined

--- Mohammed/index.js
+++ Mohammed/index.js
@@ -1,45 +1,49 @@
+
 // 1.
+
 lowerLimitNum = 50;
 upperLimitNum = 99;
 
-let firstNum = 10;
-let secondNum = 20;
+let firstNum = 40;
+let secondNum = 10;
 
-/* if(firstNum >= lowerLimitNum && firstNum <= upperLimitNum)
-{
-    console.log(firstNum >= lowerLimitNum && firstNum <= upperLimitNum);
-    if(secondNum >= lowerLimitNum && secondNum <= upperLimitNum)
-    {
-        console.log(`The numbers ${firstNum} and ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum}`);
-    }
-}   */
-
-if ( ((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) &&    
+if ( ((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||  
    ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum)) )
 {
-    console.log(`The numbers ${firstNum} and ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) &&    
+    console.log(`The numbers ${firstNum} OR ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
     ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum))} `);
 }
 else
 {
-    console.log( `The numbers ${firstNum} and ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) &&    
+    console.log( `The numbers ${firstNum} OR ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) &&    
     ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum))} ` );
 }
 
+// =========================================================================
+
 // 2-
+
+let thirdNum = 5;
+
 if ( ((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
-   ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum)) )
+     ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum)) ||  
+     ((thirdNum >= lowerLimitNum) && (thirdNum <= upperLimitNum)) )
 {
-    console.log(`The numbers ${firstNum} and ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
-    ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum))} `);
+    console.log(`One or more of the given numbers ${firstNum} or ${secondNum} or ${thirdNum} is/are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
+        ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum)) ||  
+        ((thirdNum >= lowerLimitNum) && (thirdNum <= upperLimitNum))} `);
 }
 else
 {
-    console.log(`The numbers ${firstNum} and ${secondNum} are in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
-    ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum))} `);
+    console.log(`None of the given numbers ${firstNum} or ${secondNum} or ${thirdNum} is in the range between ${lowerLimitNum} and ${upperLimitNum} ==> ${((firstNum >= lowerLimitNum) && (firstNum <= upperLimitNum)) ||    
+        ((secondNum >= lowerLimitNum) && (secondNum <= upperLimitNum)) ||  
+        ((thirdNum >= lowerLimitNum) && (thirdNum <= upperLimitNum))} `);
 }
 
+// ===================================================================
+
 // 3-
+
 let fNum = 10;
 let sNum = 30;
 let tNum = 20;
@@ -57,9 +61,29 @@ else
     console.log(`${tNum} is the greatest number among ${fNum}, ${sNum}, ${tNum}`);
 }
 
-// 4- 
+// ==========================================================================
+
+// 4-
+
+let myStr = 'hello';
+let subStr ='Py';
+let concatenatedStr; 
+
+if ( myStr.includes(subStr) )
+{
+    console.log(myStr)
+}
+else
+{
+    concatenatedStr = subStr.concat(myStr);
+    console.log(concatenatedStr);
+}
+
+
+// ==========================================================================
 
 // 5-
+
 let intN1 = 40;
 let intN2 = 70;
 
@@ -74,21 +98,27 @@ else
     console.log(80);
 }
 
+// ===========================================================================
+
 // 6-
+
 let nbr1 = 5;
 let nbr2 = 3;
 
-if ( ( (nbr1 + nbr2) === 8)  || ( (nbr1 - nbr2) === 8) )
+if ( ( (nbr1 + nbr2) === 8)  || Math.abs( nbr1 - nbr2) === 8) 
 {
     /* console.log(( (nbr1 + nbr2) === 8)  || ( (nbr1 - nbr2) === 8)); */
-    console.log(`(${nbr1} + ${nbr2}) OR (${nbr2} - ${nbr1}) = 8 ==> ${( (nbr1 + nbr2) === 8)  || ( (nbr1 - nbr2) === 8)}`)
+    console.log(`(${nbr1} + ${nbr2}) OR (${nbr2} - ${nbr1}) = 8 ==> ${( (nbr1 + nbr2) === 8)  || ( (nbr1 - nbr2) === 8)}`);
 } 
 else
 {
-    console.log(`(${nbr1} + ${nbr2}) OR (${nbr2} - ${nbr1}) = 8 ==> ${( (nbr1 + nbr2) === 8)  || ( (nbr1 - nbr2) === 8)}`)
+    console.log(`(${nbr1} + ${nbr2}) OR (${nbr2} - ${nbr1}) = 8 ==> ${( (nbr1 + nbr2) === 8)  ||  Math.abs(nbr1 - nbr2) === 8}`);
 }
 
+// =====================================================================
+
 // 7-
+
 let numb1 = 8;
 let numb2 = 7;
 
@@ -99,18 +129,23 @@ if ( (numb1 === 15) || (numb2 === 15) || ((numb1 + numb2) === 15) )
     console.log(` (${numb1} = 15) ==> ${(numb1 === 15)} \t (${numb2} = 15) ==> ${(numb2 === 15)} \t  (${numb1} + ${numb2}) = 15  ==> ${((numb1 + numb2) === 15)} \t The expression evaluation is: ${(numb1 === 15) || (numb2 === 15) || ((numb1 + numb2) === 15)}`);
 }
 
+// =================================================================
+
 // 8-
+
 let n1 = 77;
 let n2 = 20;
 
-if ( ((n1 % 7) === 0) || ((n2 % 11) === 0) )
+if ( ( ((n1 % 7) === 0) || ((n1 % 11) === 0) ) || ( ((n2 % 11) === 0) || ((n2 % 7) === 0) ) )
 {
-    console.log(`${n1} is multiple of 7 ==> ${((n1 % 7) === 0)} || ${n2} is multiple of 11 ==> ${((n2 % 11) === 0)} The expression is evaluated as  ${((n1 % 7) === 0) || ((n2 % 11) === 0) }`);
+    console.log(`${n1} is multiple of 7 ==> ${((n1 % 7) === 0)} || ${n2} is multiple of 11 ==> ${((n2 % 11) === 0)} The expression is evaluated as  ${( ((n1 % 7) === 0) || ((n1 % 11) === 0) ) || ( ((n2 % 11) === 0) || ((n2 % 7) === 0)) }`);
 }
 else
 {
     console.log(`${n1} is multiple of 7 ==> ${((n1 % 7) === 0)} || ${n2} is multiple of 11 ==> ${((n2 % 11) === 0)} The expression is evaluated as  ${((n1 % 7) === 0) || ((n2 % 11) === 0) }`);
 }
+
+// ==========================================================================
 
 // 9-
 
@@ -122,10 +157,15 @@ sum = no1 + no2;
 let eval = (no1 === no2) ? `${Math.pow(sum, 3)}` :  null;
 console.log(eval);
 
+// ================================================================================
+
 // 10-
 let theNumber = 23;
-let  difference = theNumber - 19;
+let  difference = Math.abs(theNumber - 19);
 
 let doubleNo = (theNumber > 19) ? `${Math.pow(difference, 2)}` : null;
 console.log(doubleNo);
+
+// ======================================================================
+
 // 11- Bonus --- done
